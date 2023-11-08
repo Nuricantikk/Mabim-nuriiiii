9a908998ad1ad10c273c51b5b49656505bb9da3c
Nuricantikk <148602051+Nuricantikk@users.noreply.github.com>
2023-11-08 17:30:18 +0700

    Add files via upload

---
+++ nuri.js
@@ -0,0 +1,17 @@
+function Login() {
+    var done = 0;
+    var username = document.login.username.value;
+    var password = document.login.password.value;
+    if (username == "Nuri" && password =="Nuri"){
+            if(confirm("Apakah Anda Ingin Login?") == true){
+                window.location.href = "https://github.com/Nuricantikk"
+            } else {
+                alert("Login dibatalkan karena password salah!");
+            }
+            
+    } else{
+            if (done == 0) {
+                alert("Username/Pasword Salah");
+            }
+    }
+} 
